85388cf7e5427d6321b3f126402db1b55d71bd13
Mathis Chagneux <mathis.chagneux@telecom-paris.fr>
2021-08-25 17:36:41 +0200

    minor fix

--- surfrider_sort.py
+++ surfrider_sort.py
@@ -7,8 +7,8 @@ import pickle
 
 
 
-detection_dir = '/home/infres/chagneux/repos/FairMOT/surfrider_short_segments_6fps'
-output_dir = 'surfrider_short_segments_6fps'
+detection_dir = '../../data/external_detections/FairMOT/new_short_segments_12fps'
+output_dir = 'new_short_segments_12fps'
 detection_subdirs = next(os.walk(detection_dir))[1]
 detection_filenames = [os.path.join(detection_dir,detection_subdir,'saved_detections.pickle') for detection_subdir in detection_subdirs]
 
@@ -31,28 +31,29 @@ for detection_filename in detection_filenames:
 
     tracklets = defaultdict(list)
 
+    if len(seq_dets):
+
+        for frame in range(int(seq_dets[:,0].max())):
+            frame += 1 #detection and frame numbers begin at 1
+            dets = seq_dets[seq_dets[:, 0]==frame, 2:7]
+            dets[:, 2:4] += dets[:, 0:2] #convert to [x1,y1,w,h] to [x1,y1,x2,y2]
+            total_frames += 1
 
 
-    for frame in range(int(seq_dets[:,0].max())):
-        frame += 1 #detection and frame numbers begin at 1
-        dets = seq_dets[seq_dets[:, 0]==frame, 2:7]
-        dets[:, 2:4] += dets[:, 0:2] #convert to [x1,y1,w,h] to [x1,y1,x2,y2]
-        total_frames += 1
+            start_time = time.time()
+            trackers = mot_tracker.update(dets)
+            cycle_time = time.time() - start_time
+            total_time += cycle_time
+            
+            for d in trackers:
+                track_id = int(d[4])
+                left, top, width, height = d[0],d[1],d[2]-d[0],d[3]-d[1]
+                center_x, center_y = left+width/2, top+height/2
+                tracklets[track_id].append((frame, center_x, center_y))
 
+        print("Total Tracking took: %.3f seconds for %d frames or %.1f FPS" % (total_time, total_frames, total_frames / total_time))
 
-        start_time = time.time()
-        trackers = mot_tracker.update(dets)
-        cycle_time = time.time() - start_time
-        total_time += cycle_time
-        
-        for d in trackers:
-            track_id = int(d[4])
-            left, top, width, height = d[0],d[1],d[2]-d[0],d[3]-d[1]
-            center_x, center_y = left+width/2, top+height/2
-            tracklets[track_id].append((frame, center_x, center_y))
-
-    print("Total Tracking took: %.3f seconds for %d frames or %.1f FPS" % (total_time, total_frames, total_frames / total_time))
-
+    else: print('No detections in file')
     tracklets = [tracklet for tracklet in list(tracklets.values())]
     results = []
 
